fd385c0842f7f14ecdf93da17012018607b0de99
Victor Hugo <vitorneiva_1@outlook.com>
2020-04-02 22:36:17 -0300

    Atualização

--- src/app/autenticacao/cadastro-pj/cadastro-pj.module.ts
+++ src/app/autenticacao/cadastro-pj/cadastro-pj.module.ts
@@ -3,13 +3,29 @@ import { CommonModule } from '@angular/common';
 import { CadastrarPjComponent } from './components';
 import { RouterModule } from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from  '@angular/material/list';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { CadastroPjComponent } from '../login/components/cadastro-pj.component';
 
 @NgModule({
     imports: [
         CommonModule,
         RouterModule,
-        FlexLayoutModule
+        FlexLayoutModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        MatInputModule,
+        MatButtonModule,
+        MatListModule,
+        MatTooltipModule,
+        MatIconModule,
+        MatSnackBarModule,
     ],
     declarations: [
         CadastrarPjComponent,
--- src/app/autenticacao/cadastro-pj/components/cadastrar-pj/cadastrar-pj.component.ts
+++ src/app/autenticacao/cadastro-pj/components/cadastrar-pj/cadastrar-pj.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { FormBuilder , FormGroup, Validators } from '@angular/forms';
+import { CadastroPj } from './models/cadastro-pj.model';
 
 @Component({
   selector: 'app-cadastrar-pj',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarPjComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  
+  constructor(
+    private fb: FormBuilder,
+    private snackBar: MatSnackBar,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.gerarForm();
+  }
+
+  //Validação dos formulários
+  gerarForm(){
+    this.form = this.fb.group({
+      nome: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]],
+      cpf: ['', [Validators.required]],
+      razaoSocial: ['', [Validators.required, Validators.minLength(5)]],
+      cnpj: ['', [Validators.required]]
+    });
+  }
+
+  //Condição para verificar se o formulário está preenchido
+  cadastrarPj(){
+    if(this.form.invalid){
+      return;
+    }
+    const cadastroPj: CadastroPj = this.form.value;
+    alert(JSON.stringify(this.form.value));//converter objeto Json em um String
   }
 
 }
